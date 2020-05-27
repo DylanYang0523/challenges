fea90d1a16a80ebe1f0c3318d97906d82191826b
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-05-27 21:12:59 +0800

    feat: add isValidBST.js

---
+++ isValidBST.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    let isValid = true;
+    
+    const isValidBSTRecursion = (node, min, max) => {
+        if (!node || !isValid) return;
+        if (node.val > min && node.val < max) {
+            isValidBSTRecursion(node.left, min, node.val);
+            isValidBSTRecursion(node.right, node.val, max);
+        } else {
+            isValid = false;
+        }
+    }
+    
+    isValidBSTRecursion(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+    
+    return isValid;
+};
