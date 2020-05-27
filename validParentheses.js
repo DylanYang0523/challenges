8afbd7804823a05cc5859feb6be806c56e6b257b
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-05-27 18:13:15 +0800

    feat: add validParentheses.js

---
+++ validParentheses.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    // 1. convert s -> arr
+    // 2. go through the arr and do comparison to see if it needs to put into the orphan arr
+    // 3. if orphan is empty, then the input is the valid string
+    
+    const arr = s.split('');
+    if (arr.length === 0) return true;
+    if (arr.length % 2 !== 0) return false;
+    
+    const orphanArr = [];
+    
+    const charTable = {
+        '}':'{',
+        ']':'[',
+        ')':'(',
+    }
+    
+    for(let i = 0; i < arr.length; i++) {
+        const pairValue = charTable[arr[i]];
+        if (!pairValue) {
+            orphanArr.push(arr[i]);
+        } else {
+            if (orphanArr[orphanArr.length - 1] !== pairValue) {
+                orphanArr.push(arr[i]);
+                break;
+            }
+            orphanArr.pop();
+        }
+    }
+    
+    return orphanArr.length === 0;
+};
