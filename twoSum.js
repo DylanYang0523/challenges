ada4816ce6a12c53b68f0ba65977f9927df75c41
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-05-28 15:20:17 +0800

    feat: add twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const solutionA = (nums, target) => {
+        for(let i = 0; i < nums.length - 1; i++) {
+            for(let j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) === target) {
+                    return [i, j];
+                }
+            }
+        }
+    }
+    
+    const solutionB = (nums, target) => {
+        const map = {};
+        for(let i = 0; i < nums.length; i++) {
+            const complement = target - nums[i];
+            if(map[complement] !== undefined) {
+                return [i, map[complement]];
+            }
+            map[nums[i]] = i;
+        }
+    }
+    
+    return solutionA(nums, target);
+};
