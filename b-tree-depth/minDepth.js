c8da9e9218255f8e3eb564d2170ea49db9224cde
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-05-30 11:02:09 +0800

    feat: add minDepth.js

---
+++ b-tree-depth/minDepth.js
@@ -0,0 +1,44 @@
+/**
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (!root) return 0;
+    
+    let min = 0;
+    const queue = [
+        {
+            deep: 1,
+            node: root
+        }
+    ];
+    
+    while(queue.length > 0) {
+        const obj = queue.shift();
+        const node = obj.node;
+        const deep = obj.deep;
+        if (!node.left && !node.right) {
+            min = deep;
+            break;
+        }
+        if (node.left) queue.push({
+            deep: deep + 1,
+            node: node.left
+        })
+        if (node.right) queue.push({
+            deep: deep + 1,
+            node: node.right
+        })
+    }
+    
+    return min;
+};
