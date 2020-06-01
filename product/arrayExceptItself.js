7891c426afb8a0f47fbedbf458b58141ee52a7d3
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-01 10:23:50 +0800

    feat: add product/arrayExceptItself.js

---
+++ product/arrayExceptItself.js
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/product-of-array-except-self/
+ * 238. Product of Array Except Self
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const output = [];
+    
+    const solutionA = () => {
+        const getPostsum = (presum, i) => {
+            if (i >= nums.length) return 1;
+            const num = nums[i];
+            const postsum = getPostsum(presum * num, i + 1);
+            output[i] = presum * postsum;
+            return postsum * num;
+        }
+        getPostsum(1, 0);
+    }
+    
+    const solutionB = () => {
+        let productsLeftOfMe = 1
+        for(let i = 0; i < nums.length; i++){
+            output[i] = productsLeftOfMe
+            productsLeftOfMe *= nums[i]
+        }
+        let productsRightOfMe = 1
+        for(let i = nums.length-1; i >= 0; i--){
+            output[i] *= productsRightOfMe
+            productsRightOfMe *= nums[i]
+        }
+    }
+    
+    solutionA();
+    
+    return output;
+};
