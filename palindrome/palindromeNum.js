00d27d477cd128145cffcb5db1be448984a1f245
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-01 16:23:02 +0800

    feat: add palindrom/palindromeNum.js

---
+++ palindrome/palindromeNum.js
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/palindrome-number/
+ * 9. Palindrome Number
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) return false;
+    
+    const xStr = x.toString();
+    let pointerA = 0;
+    let pointerB = xStr.length - 1;
+    
+    let isValid = true;
+    while (pointerB > pointerA) {
+        if (xStr[pointerA] === xStr[pointerB]) {
+            pointerA += 1;
+            pointerB -= 1;
+        } else {
+            isValid = false;
+            break;
+        }
+    }
+    
+    return isValid;
+};
