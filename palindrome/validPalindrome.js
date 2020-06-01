c2eeaee76b732bfa143bc7f4d3ddd0d749253e76
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-01 16:48:30 +0800

    feat: add validPalindrome.js

---
+++ palindrome/validPalindrome.js
@@ -0,0 +1,36 @@
+/**
+ * https://leetcode.com/problems/valid-palindrome/
+ * 125. Valid Palindrome
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    const regexp = new RegExp(/\w/);
+    
+    let pointerA = 0;
+    let pointerB = s.length - 1;
+    let isValid = true;
+    
+    s = s.toUpperCase();
+    
+    while (pointerB > pointerA) {
+        if (!regexp.test(s[pointerB])) {
+            pointerB -= 1;
+            continue;
+        }
+        if (!regexp.test(s[pointerA])) {
+            pointerA += 1;
+            continue;
+        }
+        if (s[pointerA] === s[pointerB]) {
+            pointerA += 1;
+            pointerB -= 1;
+            continue;
+        } else {
+            isValid = false;
+            break;
+        }
+    }
+    
+    return isValid;
+};
