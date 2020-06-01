dbb7bae6cc3a7026b601b9a6bdbf8cd64b39ce88
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-01 16:04:51 +0800

    feat: add largestNum.js

---
+++ largestNum.js
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/largest-number/
+ * 179. Largest Number
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    const newNums = nums.map((num) => num.toString());
+    
+    newNums.sort((a, b) => {
+        const aLen = a.length;
+        const bLen = b.length;
+        const maxLen = aLen + bLen;
+        for (let i = 0; i < maxLen; i++) {
+            if (a[i] === undefined) a = a.concat(a);
+            if (b[i] === undefined) b = b.concat(b);
+            if (a[i] > b[i]) {
+                return -1;
+            } else if (a[i] < b[i]) {
+                return 1;
+            } else {
+                continue;
+            }
+        }
+        return 0;
+    });
+    
+    if (newNums[0] === '0') return '0';
+    return newNums.join('');
+};
