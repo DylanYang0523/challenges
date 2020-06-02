a4e5479316a7d38b5e3d6be38ad026059dea67dc
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-02 10:06:30 +0800

    feat: add numberOfLands.js

---
+++ numberOfLands.js
@@ -0,0 +1,57 @@
+/**
+ * https://leetcode.com/problems/number-of-islands/
+ * 200. Number of Islands
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    if (!grid || grid.length < 1) return 0;
+    
+    // 1. go through matrix
+    // 2. if find a land, then count plus one ann also explore it, set all explored land as undefined
+    
+    const iBoundry = grid.length;
+    const jBoundry = grid[0].length;
+    const checkBoundry = (i, j) => {
+        if (i < iBoundry && 
+            i >= 0 &&
+            j < jBoundry &&
+            j >= 0
+           ) return true;
+        return false;
+    }
+    
+    const explore = (i, j) => {
+        // right
+        if (checkBoundry(i, j + 1) && grid[i][j + 1] === '1') {
+            grid[i][j + 1] = undefined;
+            explore(i, j + 1);
+        }
+        // left
+        if (checkBoundry(i, j - 1) && grid[i][j - 1] === '1') {
+            grid[i][j - 1] = undefined;
+            explore(i, j - 1);
+        }
+        // top
+        if (checkBoundry(i - 1, j) && grid[i - 1][j] === '1') {
+            grid[i - 1][j] = undefined;
+            explore(i - 1, j);
+        }
+        // down
+        if (checkBoundry(i + 1, j) && grid[i + 1][j] === '1') {
+            grid[i + 1][j] = undefined;
+            explore(i + 1, j);
+        }
+    }
+    
+    let landCount = 0;
+    for (let i = 0; i < iBoundry; i++) {
+        for (let j = 0; j < jBoundry; j++) {
+            if (grid[i][j] === undefined || grid[i][j] === '0') continue;
+            landCount += 1;
+            explore(i, j);
+        }
+    }
+    
+    return landCount;
+};
