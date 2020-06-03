92c926f0fb61680c144b64589e315ec5e15411cd
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-03 10:31:33 +0800

    feat: add monotonic-array/isValidMonotonic.js

---
+++ monotonic-array/isValidMonotonic.js
@@ -0,0 +1,34 @@
+/**
+ * https://leetcode.com/problems/monotonic-array/
+ * 896. Monotonic Array
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var isMonotonic = function(A) {
+    if (!A || A.length < 1) return false;
+    if (A.length === 1) return true;
+    
+    // 1 increasing, 0 decreasing
+    let direction;
+    let isValid = true;
+    for (let i = 1; i < A.length; i++) {
+        const diff = A[i] - A[i-1];
+        if (direction === undefined) {
+            if (diff > 0) {
+                direction = 1;
+            } else if (diff < 0) {
+                direction = 0;
+            } else {}
+        } else if (direction === 1) {
+            if (diff >= 0) continue;
+            isValid = false;
+            break;
+        } else {
+            if (diff <= 0) continue;
+            isValid = false;
+            break;
+        }
+    }
+    
+    return isValid;
+};
