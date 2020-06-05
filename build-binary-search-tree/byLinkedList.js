c231fd7b0c2ac4d3b3e62266aa2d3bbd77be76ba
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-05 09:40:38 +0800

    feat: add byLinkedList.js

---
+++ build-binary-search-tree/byLinkedList.js
@@ -0,0 +1,49 @@
+/**
+ * https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+ * 109. Convert Sorted List to Binary Search Tree
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function(head) {
+    if (!head) return null;
+    
+    const arr = [];
+    
+    const buildArr = (node) => {
+        arr.push(node.val);
+        if (node.next) buildArr(node.next);
+    }
+    buildArr(head);
+    
+    const buildTree = (startIdx, endIdx) => {
+        if (startIdx > endIdx) return null;
+        
+        const mid = Math.ceil((startIdx + endIdx) / 2);
+        const midVal = arr[mid];
+        
+        const node = new TreeNode(midVal, null, null);
+        node.left = buildTree(startIdx, mid - 1);
+        node.right = buildTree(mid + 1, endIdx);
+        
+        return node;
+    }
+    
+    const root = buildTree(0, arr.length - 1);
+    
+    return root;
+};
