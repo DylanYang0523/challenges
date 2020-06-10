0916e6d1597b9469fb3ef03a0253232815756b0e
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-10 10:01:58 +0800

    feat: add nAryTree.js

---
+++ BFS/nAryTree.js
@@ -0,0 +1,57 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * https://leetcode.com/problems/n-ary-tree-level-order-traversal/
+ * 429. N-ary Tree Level Order Traversal
+ * @param {Node} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    const solutionA = () => {
+        // deep first with recursion
+        if (!root) return [];
+        const ans = [];
+        const deep = (node, level) => {
+            if (ans[level] === undefined) ans[level] = [];
+            ans[level].push(node.val);
+            if (node.children) {
+                for (let i = 0; i < node.children.length; i++) {
+                    deep(node.children[i], level + 1);
+                }   
+            }
+        }
+        
+        deep(root, 0);
+        return ans;
+    }
+    
+    const solutionB = () => {
+        // breadth first
+        if (!root) return [];
+        const ans = [];
+        const queue = [root];
+        while (queue.length !== 0) {
+            const len = queue.length;
+            const tmp = [];
+            for (let i = 0; i < len; i++) {
+                const curr = queue.shift();
+                tmp.push(curr.val);
+                if (curr.children) {
+                    for (let j = 0; j < curr.children.length; j++) {
+                        queue.push(curr.children[j]);
+                    }   
+                }
+            }
+            ans.push(tmp);
+        }
+        return ans;
+    }
+    
+    return solutionB();
+};
