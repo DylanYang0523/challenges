1e8f501e8771f4b22e8625c44f260810fd56ec47
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-11 14:23:05 +0800

    feat: add stream/kthLargest.js

---
+++ stream/kthLargest.js
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/kth-largest-element-in-a-stream/
+ * 703. Kth Largest Element in a Stream
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+    this.k = k;
+    this.stream = getStream(nums) || [];
+    
+    function getStream(nums) {
+        nums.sort((a, b) => (b - a));
+        return nums.slice(0, k).sort((a, b) => (a - b));
+    }
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    if (this.stream.length < this.k) {
+        this.stream.unshift(val);
+        this.stream.sort((a, b) => (a - b));
+    } else if (val > this.stream[0]) {
+        this.stream.shift();
+        this.stream.unshift(val);
+        this.stream.sort((a, b) => (a - b));
+    }
+    return this.stream[0];
+};
+
+/** 
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
