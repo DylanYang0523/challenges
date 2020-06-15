64cbb24a8879ae642e1d9fc92b065c0f718cbbd6
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-15 15:25:49 +0800

    feat: add minStackConstruction.js

---
+++ minStackConstruction.js
@@ -0,0 +1,58 @@
+// https://leetcode.com/problems/min-stack/submissions/
+// 155. Min Stack
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.stack = [];
+    this.min = undefined;
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    this.stack.push(x);
+    if (this.min !== undefined) {
+        this.min = this.min > x ? x : this.min;
+    } else {
+        this.min = x;
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    const curr = this.stack.pop();
+    if (curr !== undefined && this.min === curr) {
+        const tmp = [...this.stack];
+        tmp.sort((a, b) => (a - b));
+        this.min = tmp[0];
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    if (this.stack.length > 0) return this.stack[this.stack.length - 1];
+    return null;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.min;
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
