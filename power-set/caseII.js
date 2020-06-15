ca687725b18a91ccee71910a2bdc9f6db888db17
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-15 11:32:43 +0800

    feat: add power-set/caseII.js

---
+++ power-set/caseII.js
@@ -0,0 +1,47 @@
+/**
+ * https://leetcode.com/problems/subsets-ii/
+ * 90. Subsets II
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    const solutionA = () => {
+        const ans = [];
+        const map = {};
+        const getSub = (prev, remain) => {
+            for (let i = 0; i < remain.length; i++) {
+                const curr = remain.slice(i, i + 1);
+                const next = remain.slice(i + 1);
+                let newP = [...prev];
+                newP = newP.concat(curr);
+                newP.sort((a, b) => (a - b));
+
+                const pKey = newP.join('');
+                if (map[pKey]) continue;
+
+                map[pKey] = true;
+                ans.push(newP);
+                getSub(newP, next);
+            }
+        }
+        getSub([], nums);
+        ans.push([]);
+        return ans;
+    }
+    
+    const solutionB = () => {
+        nums.sort((a, b) => (a - b));
+        const ans = [];
+        const getSub = (prev, remain) => {
+            ans.push(prev);
+            for (let i = 0; i < remain.length; i++) {
+                if (remain[i] === remain[i - 1]) continue;
+                getSub([...prev, remain[i]], remain.slice(i + 1));
+            }
+        }
+        getSub([], nums);
+        return ans;
+    }
+    
+    return solutionB();
+};
