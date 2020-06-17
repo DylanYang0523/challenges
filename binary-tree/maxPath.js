146619bec6beb1f0712069b63dba80bb402b9f0a
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-17 11:06:55 +0800

    feat: add maxPath.js

---
+++ binary-tree/maxPath.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/
+ * 124. Binary Tree Maximum Path Sum
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    if (!root) return null;
+    
+    let max = Number.MIN_SAFE_INTEGER;
+    const findM = (node) => {
+        if (!node) return Number.MIN_SAFE_INTEGER;
+        const l = findM(node.left);
+        const r = findM(node.right);
+        
+        // path case
+        const val = node.val;
+        const pathM = Math.max(l + val + r, l + val, r + val, val);
+        max = Math.max(max, pathM);
+        
+        // return case
+        const currMax = Math.max(l + val, r + val, val);
+        return currMax;
+    }
+    findM(root);
+    return max;
+};
