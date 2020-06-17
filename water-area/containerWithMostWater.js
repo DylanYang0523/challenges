de97356876be25a0a8f5a88b8afb3683abb31746
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-17 16:49:54 +0800

    feat: add water-area/containerWithMostWater.js

---
+++ water-area/containerWithMostWater.js
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/container-with-most-water/
+ * 11. Container With Most Water
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let pointerA = 0;
+    let pointerB = height.length - 1;
+    let max = Number.MIN_SAFE_INTEGER;
+    while (pointerA < pointerB) {
+        const h = Math.min(height[pointerA], height[pointerB]);
+        const w = pointerB - pointerA;
+        const area = h * w;
+        max = Math.max(area, max);
+        if (height[pointerA] === h) {
+            pointerA += 1;
+        } else {
+            pointerB -= 1;
+        }
+    }
+    return max;
+};
