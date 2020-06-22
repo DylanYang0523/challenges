05290a9be0b011810a03d35a0ef83ee62a6bb2d1
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-22 18:33:31 +0800

    feat: add longestUniqueSubString.js

---
+++ longestUniqueSubString.js
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * 3. Longest Substring Without Repeating Characters
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const m = new Map();
+    let max = Number.MIN_SAFE_INTEGER;
+    let start = 0;
+    for (let i = 0; i < s.length; i++) {
+        const prev = m.get(s[i]);
+        if (prev !== undefined) {
+            const l = i - start;
+            max = Math.max(max, l);
+            if (prev + 1 > start) {
+                start = prev + 1;   
+            }
+            m.set(s[i], i);
+        } else {
+            m.set(s[i], i);
+        }
+    }
+    max = Math.max(max, s.length - start);
+    return max;
+};
