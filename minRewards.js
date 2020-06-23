dccc759bc628cf7f279f6e30677e908c78ea027f
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-23 12:33:39 +0800

    feat: add minRewards.js

---
+++ minRewards.js
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/candy/
+ * 135. Candy
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+    const arr = Array(ratings.length).fill(1);
+    for (let i = 1; i < ratings.length; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            const tmp = arr[i - 1] + 1;
+            arr[i] = Math.max(arr[i], tmp);
+        }
+    }
+    for (let i = ratings.length - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            const tmp = arr[i + 1] + 1;
+            arr[i] = Math.max(arr[i], tmp);
+        }
+    }
+    let ans = 0;
+    for (let i = 0; i < arr.length; i++) {
+        ans += arr[i];
+    }
+    return ans;
+};
