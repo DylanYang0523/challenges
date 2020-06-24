6c37dd9561f1d03fa613d776dcd58f8f79f06dc6
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-24 15:46:04 +0800

    feat: add maxProfitWithCooldown.js

---
+++ maxProfitWithCooldown.js
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ * 309. Best Time to Buy and Sell Stock with Cooldown
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let hold = Number.MIN_SAFE_INTEGER, sold = 0, cool = 0;
+    for (let i = 0; i < prices.length; i++) {
+        let preHold = hold,
+            preSold = sold,
+            preCool = cool;
+        
+        hold = Math.max(preHold, preCool - prices[i]);
+        sold = preHold + prices[i];
+        cool = Math.max(preCool, preSold);
+    }
+    return Math.max(sold, cool);
+};
