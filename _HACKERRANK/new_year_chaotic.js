7879957ef3c28acd3fd2d07743c7a0f54375f57b
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-07-08 17:01:29 +0800

    feat: add new_year_chaotic.js

---
+++ _HACKERRANK/new_year_chaotic.js
@@ -0,0 +1,62 @@
+/**
+ * @lv 🔴
+ * @link https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+ * @name New Year Chaos
+ */
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumBribes function below.
+function minimumBribes(q) {
+    let swaps = 0;
+    let min = q.length;
+    for (var i = q.length - 1; i >= 0; i--){
+        if (q[i] - i > 3){
+            console.log(`Too chaotic`);
+            return;
+        }
+        if (q[i] > i+1){
+            swaps += (q[i]-(i+1));
+        } else {
+            if (min > q[i]){
+                min = q[i];
+            } else if (q[i] != min){
+                swaps++;
+            }
+        }
+    }
+    console.log(swaps);
+}
+
+function main() {
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine(), 10);
+
+        const q = readLine().split(' ').map(qTemp => parseInt(qTemp, 10));
+
+        minimumBribes(q);
+    }
+}
