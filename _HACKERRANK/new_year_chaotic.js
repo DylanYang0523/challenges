24eb49c2a84a589a33271d5bf731192dd4252275
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-07-08 17:02:31 +0800

    feat: add a category to new_year_chaotic.js

--- _HACKERRANK/new_year_chaotic.js
+++ _HACKERRANK/new_year_chaotic.js
@@ -2,6 +2,7 @@
  * @lv 🔴
  * @link https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
  * @name New Year Chaos
+ * @cate #editor_pick
  */
 'use strict';
 
